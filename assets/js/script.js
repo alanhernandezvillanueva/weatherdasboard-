17908b68bffe0f227c29bf26139545219b0ca50d
alanhernandezvillanueva <79774478+alanhernandezvillanueva@users.noreply.github.com>
2021-11-18 21:54:45 -0800

    added fucntionality to jump between cities and also display the next five days weather

---
+++ assets/js/script.js
@@ -0,0 +1,114 @@
+var searchButtonEl = document.querySelector(".btn")
+var currentCityNameEl = document.querySelector("#current-city-name");
+var currentCityWeatherEl = document.querySelector("#current-weather");
+var searchCityEl = document.querySelector("#entercity");
+var fiveWeatherEl = document.querySelector("#five-day-weather")
+var dict = {};
+//create a fucntion to accept data input 
+
+getFiveDayWeatherData = function (city) {
+    var fiveDayWeatherApi = `HTTPS://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=78997b58c6d829d305a731ef5afb29ed`;
+    fetch(fiveDayWeatherApi).then(function (response) {
+        return response.json()
+         }).then(function (data) {
+             console.log(data);
+             showFiveDayForecast(data);
+             
+         })
+        }
+    
+
+//create an API call to access data 
+var getWeatherData = function(city){
+    var apiUrl = `HTTPS://api.openweathermap.org/data/2.5/weather?q=${city}&appid=78997b58c6d829d305a731ef5afb29ed`;
+    fetch(apiUrl).then(function (response) {
+        return response.json()
+    }) .then(function (data) {
+        console.log(data);
+        displayCityWeather(data);
+    });
+    
+}
+searchButtonEl.addEventListener("click", function(event) {
+    event.preventDefault()
+    var typedCity = searchCityEl.value;
+    getWeatherData(typedCity);
+    getFiveDayWeatherData(typedCity);
+    searchCityEl.value= "";
+     var cityName = document.createElement("button");
+     cityName.classList.add("cityNameBtn")
+     cityName.textContent = typedCity;
+     currentCityNameEl.append(cityName);
+    //  cityName.style.width = "220%"
+    //  cityName.style.padding= "20px"
+    //  cityName.style.fontSize="40px";
+    //  cityName.style.borderRadius="15px"
+    //  cityName.style.marginBottom="10px";
+    //  currentCityWeatherEl.style.border="thick solid lavender";
+     
+} 
+);
+currentCityNameEl.addEventListener("click", function(event) {
+    event.preventDefault()
+    var clickedCity = event.target.textContent;
+    getWeatherData(clickedCity);
+    getFiveDayWeatherData(clickedCity);
+}
+);
+
+
+
+
+ //create a function to diplay the currentcity weather 
+ var displayCityWeather = function (data) {
+    currentCityWeatherEl.innerHTML = "";
+    currentCityWeatherEl.classList.add("currentweatherdiv")
+   
+    var cityName = document.createElement("h1");
+    cityName.textContent = data.name;
+    currentCityWeatherEl.append(cityName);
+    
+
+
+    var temperature = document.createElement("h2");
+    temperature.textContent = "Temp: " + data.main.temp;
+    currentCityWeatherEl.append(temperature);
+    temperature.style.margin = "20px 0 20px 0"
+
+    var windSpeed =  document.createElement("h2");
+    windSpeed.textContent = "Wind: " + data.wind.speed + " miles per hour";
+    currentCityWeatherEl.append(windSpeed);
+    windSpeed.style.margin = "20px 0 20px 0"
+    
+
+    
+    var cityHumidity = document.createElement("h2");
+    cityHumidity.textContent = "Humidity " + data.main.humidity + "%";
+    currentCityWeatherEl.append(cityHumidity);
+    cityHumidity.style.margin = "20px 0 20px 0"
+    //var uvIndex = ;
+   
+    //create a forloop that goes through the array 
+    
+}
+
+
+ function showFiveDayForecast(data) {
+    var selectedCity = searchCityEl.value;
+    var index = dict[selectedCity];
+    
+    
+    var fiveDaysCards = `<div><h1>${data.city.name}  Five Day Forecast</h1></div>`
+   
+        for (var i = 0; i < data.list.length; i+=8) {
+           fiveDaysCards += ` <div class="card" style="width: 18rem;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item"> Temperature: ${data.list[i].main.temp}</li>
+    <li class="list-group-item">Wind Speed: ${data.list[i].wind.speed}</li>
+    <li class="list-group-item"> Humidity: ${data.list[i].main.humidity}</li>
+  </ul>
+</div>`        
+        }
+    fiveWeatherEl.innerHTML = fiveDaysCards
+    
+ }
